a87867ea089784bd2e8f72312304b7c8be8ac663
jcarapet <jeffcarapetyan@gmail.com>
2015-07-16 12:20:34 -0500

    added syslog.conf data

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,3 @@
-default['df_logstash']['logstash_ip']= node['rackspace']['privateip']
+default['df_logstash']['logstash_ip']= 'localhost'
 default['df_logstash']['logstash_port'] = '5000'
 default['df_logstash']['ssl_path'] = '/etc/pki/tls'
