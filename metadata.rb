a65cf8664a3fd42da4cf86dc5b15a2451baf547e
jcarapet <jeffcarapetyan@gmail.com>
2015-07-16 12:12:39 -0500

    uncommented

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'you@example.com'
 license 'all_rights'
 description 'Installs/Configures df_logstash'
 long_description 'Installs/Configures df_logstash'
-version '0.1.1'
+version '0.1.2'
 
 depends 'apt'
--- recipes/logstash_forwarder.rb
+++ recipes/logstash_forwarder.rb
@@ -7,11 +7,11 @@ apt_package "logstash-forwarder" do
 	action :install
 end
 
-#need full template in system before moving forward. 
-# template "/etc/logstash-forwarder.conf" do 
-# 	source "logstash-forwarder.conf.erb" 
-# 	mode "0664" 
-# end
+# need full template in system before moving forward. 
+template "/etc/logstash-forwarder.conf" do 
+	source "logstash-forwarder.conf.erb" 
+	mode "0664" 
+end
  service "logstash-forwarder" do 
  	action :restart
  end
