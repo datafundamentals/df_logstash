3451a69c7bdd804a1e6f40a4476af193c8a08e8f
jcarapet <jeffcarapetyan@gmail.com>
2015-07-16 11:08:42 -0500

    fixed sources.list name

--- recipes/logstash_forwarder.rb
+++ recipes/logstash_forwarder.rb
@@ -1,4 +1,4 @@
-apt_repository "logstash-forwarder" do 
+apt_repository "logstashforwarder" do 
 	uri "deb http://packages.elasticsearch.org/logstashforwarder/debian stable main"
 	key "http://packages.elasticsearch.org/GPG-KEY-elasticsearch" 
 end
@@ -7,11 +7,11 @@ apt_package "logstash-forwarder" do
 	action :install
 end
 
-template "/etc/logstash-forwarder.conf" do 
-	source "logstash-forwarder.conf.erb" 
-	mode "0664" 
-end
+#need full template in system before moving forward. 
+# template "/etc/logstash-forwarder.conf" do 
+# 	source "logstash-forwarder.conf.erb" 
+# 	mode "0664" 
+# end
  service "logstash-forwarder" do 
  	action :restart
  end
- 
