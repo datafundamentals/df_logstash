47b2bce2e732a023db8f5f146b51f59a28d1351b
jcarapet <jeffcarapetyan@gmail.com>
2015-08-04 12:27:38 -0500

    reworked config for distributed setup

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,4 +2,4 @@ default['df_logstash']['logstash_ip']= '10.0.1.2'
 default['df_logstash']['logstash_port'] = '5000'
 default['df_logstash']['ssl_path'] = '/etc/pki/tls'
 
-default['df_logstash']['elasticsearch_host'] = 'localhost'
+default['df_logstash']['elasticsearch_host'] = "localhost"
