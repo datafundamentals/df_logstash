e76c88fa875230d29fd667a5d046c4f795a37264
jcarapet <jeffcarapetyan@gmail.com>
2015-08-05 17:17:09 -0500

    adjusted config file in logstash output

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'you@example.com'
 license 'all_rights'
 description 'Installs/Configures df_logstash'
 long_description 'Installs/Configures df_logstash'
-version '0.1.4'
+version '0.1.5'
 
 depends 'apt'
